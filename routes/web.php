f718b5d3c953f2fa0dbc27dd82ea98a7566f87c9
BACK-END.03 <BACK-END.03@TURBOTECH.COM>
2024-12-17 17:27:59 +0700

    create api login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\StaffController;
+use App\Models\Staff;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('master');
 });
+
+Route::get('/staff',[StaffController::class,'index']);
